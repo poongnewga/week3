da4ad889ed59f1f52756ccb2ce9ac189eb9ba3c0
Hee Jae Chae <poongnewga@gmail.com>
2018-04-05 15:47:13 +0900

    Demo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+
   def index
+
   end
+
+  def login
+
+    @name = params[:id]
+    @code = params[:pw]
+
+    if @code != "heeham"
+      flash["err"] = "Not Invited :("
+      redirect_to "/"
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
   get 'home/index'
+
+  post 'login' => 'home#login'
+
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
